0aa3717507d988f22e73d82e7022ff8ebd68886b
username123321123321 <denn.iv.nik@gmail.com>
2022-02-13 01:13:46 +0300

    change text on css selectors

--- src/test/java/MaxilectTest.java
+++ src/test/java/MaxilectTest.java
@@ -2,35 +2,34 @@ import com.codeborne.selenide.WebDriverRunner;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Selectors.byCssSelector;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.Assert.assertEquals;
 
 public class MaxilectTest {
     String baseUrl = "https://maxilect.ru";
-    String mainMenuProjectText = "Проекты";
-    String blockchainTagText = "Blockchain";
-    String allProjectsButtonText = "Все проекты";
-    String projectsUrl = baseUrl + "/projects/";
+    String projectsUrl = baseUrl + "/projects";
+    String mainMenuProjectSelector = "li#menu-item-89";
+    String filterBlockchainSelector = ".filter[data-filter='blockchain']";
+    String allProjectsButtonSelector = "div.Load-More a[href='" + projectsUrl + "']";
     String blockchainPortfolioItemSelector = ".portfolio-item[data-filter='blockchain']";
     int blockchainProjectsCount = 2;
 
     @Test
     public void redirectToBlockchainProjectsPage() {
         open(baseUrl);
-        $(byText(mainMenuProjectText)).click();
-        assertEquals(projectsUrl, WebDriverRunner.getWebDriver().getCurrentUrl());
+        $(byCssSelector(mainMenuProjectSelector)).click();
+        assertEquals(projectsUrl + "/", WebDriverRunner.getWebDriver().getCurrentUrl());
         blockchainContentCheck();
 
         open(baseUrl);
-        $(byText(allProjectsButtonText)).scrollIntoView(false);
-        $(byText(allProjectsButtonText)).click();
-        assertEquals(projectsUrl, WebDriverRunner.getWebDriver().getCurrentUrl());
+        $(byCssSelector(allProjectsButtonSelector)).scrollIntoView(false);
+        $(byCssSelector(allProjectsButtonSelector)).click();
+        assertEquals(projectsUrl + "/", WebDriverRunner.getWebDriver().getCurrentUrl());
         blockchainContentCheck();
     }
 
     private void blockchainContentCheck() {
-        $(byText(blockchainTagText)).click();
+        $(byCssSelector(filterBlockchainSelector)).click();
         assertEquals(blockchainProjectsCount, $$(byCssSelector(blockchainPortfolioItemSelector)).size());
     }
 }
